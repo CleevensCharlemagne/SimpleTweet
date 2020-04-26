5200aee9961fa92884a4604cb58d5ff72be45fd1
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2020-04-25 22:21:24 -0400

    set up menu xml and timeline activity for floating button

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,3 +1,4 @@
+
 package com.codepath.apps.restclienttemplate;
 
 import androidx.annotation.Nullable;
@@ -6,12 +7,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -19,6 +23,7 @@ import com.codepath.apps.restclienttemplate.models.TweetDao;
 import com.codepath.apps.restclienttemplate.models.TweetWithUser;
 import com.codepath.apps.restclienttemplate.models.User;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,6 +39,8 @@ public class TimelineActivity extends AppCompatActivity {
     private static final String TAG = "TimelineActivity";
     private final int REQUEST_CODE = 20;
 
+    FloatingActionButton fab;
+
     TweetDao tweetDao;
     TwitterClient client;
     RecyclerView rvTweets;
@@ -47,8 +54,13 @@ public class TimelineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
 
+        fab = findViewById(R.id.fab);
+
         client = TwitterApp.getRestClient(this);
         tweetDao = ((TwitterApp) getApplicationContext()).getMyDatabase().tweetDao();
+
+//        ActionBar bar = getActionBar();
+//        bar.setBackgroundDrawable(new ColorDrawable("#77b5fe"));
 
         swipeContainer = findViewById(R.id.swipeContainer);
         // Configure the refreshing colors
@@ -69,10 +81,19 @@ public class TimelineActivity extends AppCompatActivity {
         // Intit the list of tweets and adapter
         tweets = new ArrayList<>();
         adapter = new TweetsAdapter(this, tweets);
-       LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         // Recycler view set up: layout manager and the adapter
         rvTweets.setLayoutManager(layoutManager);
         rvTweets.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TimelineActivity.this, ComposeActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
         scrollListener = new EndlessRecyclerViewScrollListener(layoutManager) {
             @Override
             public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
@@ -104,17 +125,17 @@ public class TimelineActivity extends AppCompatActivity {
         return true;
     }
 /*
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.compose){
-            // Compose item has been selected
-            // Navigate to the compose activity
-            Intent intent = new Intent(this, ComposeActivity.class);
-            startActivityForResult(intent, REQUEST_CODE);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+       if(item.getItemId() == R.id.compose){
+           // Compose item has been selected
+           // Navigate to the compose activity
+           Intent intent = new Intent(this, ComposeActivity.class);
+           startActivityForResult(intent, REQUEST_CODE);
+           return true;
+       }
+       return super.onOptionsItemSelected(item);
+   }*/
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -196,3 +217,4 @@ public class TimelineActivity extends AppCompatActivity {
         });
     }
 }
+ 
